7eec7edda30ca06c0c8d2c10f908f6360c8b0900
Laiba Ashar <asharmunir642@gmail.com>
2026-05-16 09:46:17 +0500

    pagination,filtering,searching,added

--- django_drf/students/views.py
+++ django_drf/students/views.py
@@ -1,17 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status,viewsets
+from rest_framework import status,viewsets,filters
 from .models import Students
 from .serializers import StudentSerializer
 
-
+#viewsets jis mai hamay automatically sara code mil jata hai put patch delte kai liye
 class StudentViewSet(viewsets.ModelViewSet):
     queryset=Students.objects.all()
     serializer_class=StudentSerializer
-""" class StudentListView(APIView):
+
+filter_backends=[DjangoFilterBackend,filters.SearchFilter]
+filterset_fields=['course','marks']
+search_fields=['name','email','course']
+ordering_fields=['marks','created_at']
+
+#Three methods there are pagination ,filtering ,searching ,pagination means kai 
+# agr 1000 data sets hai tu wo aik baar nai diye ja skty with the sets of 10 students 
+# record diye jaty hai taky server pr zyada load na ho aur filtering means
+#  kai specific data chaye like jin students kai marks 80 hai unka data jin ki skill django 
+# hai aur searching means kai hum jistrah browser pr likhty hai kai yr muje googgle ki website chaye ye search kro 
+""" 
+Api view jis mai hamay manuallyy sara code likhna prta hai dekte,post,put kai iye
+
+class StudentListView(APIView):
     def get(self,request):
         students=Students.objects.all()
         serializer=StudentSerializer(students,many=True)
